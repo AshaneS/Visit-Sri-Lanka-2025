9b34ce801587b568241746b03df7982b09fbc79b
Ashane Silva <ashanecool@gmail.com>
2025-01-31 22:18:25 +0530

    font fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Lato, Sora, Manrope, Urbanist, Public_Sans, Inter } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
-
-const lato = Lato({
-  subsets: ["latin"],
-  variable: "--font-lato",
-  weight: ["100", "300", "400", "700", "900"],
-});
+// const lato = Lato({
+//   subsets: ["latin"],
+//   variable: "--font-lato",
+//   weight: ["100", "300", "400", "700", "900"],
+// });
 
 const public_Sans = Public_Sans({
   subsets: ["latin"],
@@ -31,8 +30,8 @@ export default function RootLayout({
       <body className={`${public_Sans.className} overflow-x-hidden`}>
         <Navbar />
         {children}
-        <Footer/>
-      </body>   
+        <Footer />
+      </body>
     </html>
   );
 }

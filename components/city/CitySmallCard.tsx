bd7bf610fab222e70b6d0bb4f29d8f8b4e3d3645
Ashane Silva <ashanecool@gmail.com>
2025-01-31 23:37:32 +0530

    fixed types

--- components/city/CitySmallCard.tsx
+++ components/city/CitySmallCard.tsx
@@ -1,4 +1,9 @@
-function CitySmallCard(props) {
+interface CitySmallCardProps {
+  className?: string;
+  text: React.ReactNode;
+}
+
+function CitySmallCard(props: CitySmallCardProps) {
   return (
     <div className={`${props.className} rounded-xl hover:scale-95 transition duration-[1.2s] ease-[cubic-bezier(0.11, 0.46, 0.45, 0.94)]`}>
       <div className=" relative min-h-0 aspect-square min-w-0 ">

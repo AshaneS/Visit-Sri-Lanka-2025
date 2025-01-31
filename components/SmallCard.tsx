42fb1820ad589685c1b0bcd20eb10869eea40687
Ashane Silva <ashanecool@gmail.com>
2025-01-31 23:35:48 +0530

    fixed types

--- components/SmallCard.tsx
+++ components/SmallCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-function SmallCard({ img }) {
+function SmallCard({ img }: { img: string }) {
   return (
     <div className={` h-full flex flex-col gap-1`}>
       <div className="relative rounded-xl overflow-hidden h-full max-w-full min-w-0 aspect-square hover:scale-95 transition duration-500 ease-in-out">
@@ -17,5 +17,4 @@ function SmallCard({ img }) {
       </div>
     </div>
   );
-}
-export default SmallCard;
+}export default SmallCard;
